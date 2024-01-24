fae5ce43f91c46b2671888c11fc6cd6fb026ddc2
Tharunkumar1024 <157593725+Tharunkumar1024@users.noreply.github.com>
2024-01-24 16:19:45 +0530

    Add files via upload
    Committing the code for assignments on
    Assignment 1: Find the Broken Links in Amazon.com with Java 8 -
    For each loop
    Stream
    Parallel Stream
    Lambda expression
    Assignment 2: Write down feature file for different API calls as below using this API Portal https://restful-booker.herokuapp.com/apidoc/index.html
    with all request options in API

---
+++ BrokenLinks.java
@@ -0,0 +1,59 @@
+package org.irctc.login;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinks {
+	
+	private static String url;
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		List<WebElement> linksAmazon = driver.findElements(By.tagName("a"));
+		System.out.println("the number of links available is: " + linksAmazon.size() );
+		
+		List<String> emptyList = new ArrayList<String>();
+		
+		for(WebElement findbrokenLinks :linksAmazon) {
+			url = findbrokenLinks.getAttribute("href");
+			emptyList.add(url);
+			//CheckBrokenLinks(url);
+			}
+		
+		long StartTime = System.currentTimeMillis();
+		emptyList.parallelStream().forEach(e->CheckBrokenLinks(e));
+		long endTime = System.currentTimeMillis();
+		System.out.println("Total time taken" + (StartTime - endTime));
+		
+		driver.quit();		
+	}
+	public static void CheckBrokenLinks(String validateUrl) {
+		
+		try {
+			URL linkurl = new URL(validateUrl);
+			HttpURLConnection conn = (HttpURLConnection) linkurl.openConnection();
+			conn.setConnectTimeout(5000);
+			conn.connect();			
+			
+			if(conn.getResponseCode()>=400) {
+				System.out.println(linkurl + "--->" + conn.getResponseMessage() + "is a broken link");
+			}
+			else {
+				System.out.println(linkurl + "--->" + conn.getResponseMessage());
+			}
+			
+		}
+	catch(Exception e)
+			{
+		
+		}
+	
+	}
+}
